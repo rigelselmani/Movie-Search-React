622514f1ad220dbc35955b00bd26cf0e62eec00e
rigel selmani <rigelrigelselmani@hotmail.com>
2020-05-29 00:44:56 -0400

    header , input+button

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Heading from "./Header";
+import Input from "./Input";
+
+function App() {
+    return(
+     <div>
+          <Heading />
+        <div className="container">
+          <Input />
+        </div>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+  return (
+    <nav className="navbar justify-content-center text-white navbar-dark bg-dark">
+        <h1>Hooked</h1>
+    </nav>
+  )
+}
+
+export default Header;
